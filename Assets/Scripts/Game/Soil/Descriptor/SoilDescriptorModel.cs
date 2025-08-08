e58ea63a842bf8642aefbc2f5689474f9fce9c48
Neiran <sousovi@mail.ru>
2025-08-08 13:59:30 +0300

    Soil. Part 3. SoilDescriptor

--- Assets/Scripts/Game/Soil/Descriptor/SoilDescriptorModel.cs
+++ Assets/Scripts/Game/Soil/Descriptor/SoilDescriptorModel.cs
@@ -9,13 +9,15 @@ namespace Game.Soil.Descriptor
     {
         [field: SerializeField]
         public SoilType SoilType { get; set; }
-        [field: SerializeField]
-        public float Ph { get; set; }
-        [field: SerializeField]
-        public int Salinity { get; set; }
-        [field: SerializeField]
-        public float Breathability { get; set; }
-        [field: SerializeField]
-        public float Humus { get; set; } = 30000;
+        [field: SerializeField, Range(0f, 14f)]
+        public float Ph { get; set; } = 6.5f;
+        [field: SerializeField, Range(0f, 5f), Tooltip("Amount of salines in soil in percents. Max 5%")]
+        public float Salinity { get; set; } = 0.1f;
+        [field: SerializeField, Range(0f, 100f), Tooltip("Changes breathe for roots. Also represent amount of water, that can stay in soil (100-Breathability)")]
+        public float Breathability { get; set; } = 50f;
+        [field: SerializeField, Range(0f, 100f), Tooltip("Humus mass in percents")]
+        public float Humus { get; set; } = 10f;
+        [field: SerializeField, Tooltip("Soil mass in kilograms. Need for soil parameters calculations.")]
+        public float Mass { get; set; } = 1000f;
     }
 }
--- Assets/Scripts/Game/Soil/Model/SoilModel.cs
+++ Assets/Scripts/Game/Soil/Model/SoilModel.cs
@@ -7,5 +7,14 @@
         public float Breathability { get; set; }
         public float Humus { get; set; }
         public SoilType Type { get; set; }
+
+        public SoilModel(float ph, int salinity, float breathability, float humus, SoilType type)
+        {
+            Ph = ph;
+            Salinity = salinity;
+            Breathability = breathability;
+            Humus = humus;
+            Type = type;
+        }
     }
 }
